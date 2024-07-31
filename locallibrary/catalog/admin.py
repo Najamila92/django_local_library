7cf15f7ce538a559ef8092e851fe713d9072b642
najamila92 <najamila92@gmail.com>
2024-08-01 00:54:50 +0100

    update on admin and model

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
+from .models import Author, Book, Genre, BookInstance, Language
+
 # Register your models here.
+
+# admin.site.register(Author)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    # list view layout
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    # detail view layout
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    # Sectioning the detail view using fieldsets 
+
+
+
+# admin.site.register(BookInstance)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'due_back', 'status')
+    list_filter = ('status', 'due_back')
+
+    # Sectioning the detail view using fieldsets 
+    fieldsets = (
+        (
+            None, {'fields': ('id', 'book', 'imprint')} 
+        ), 
+        
+        (
+            'Availability', {'fields': ('status', 'due_back')}
+        ),
+    )
+
+#inline BookInstance 
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+
+
+# admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'isbn', 'display_genre')
+    inlines = [BookInstanceInline]
+
+
+admin.site.register(Genre) 
+admin.site.register(Language) 
